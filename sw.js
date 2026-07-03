098cad7eb84158be3e34f8f37650492293bca6d9
okok6868 <fjun9198@gmail.com>
2026-07-03 15:57:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yll-pos-v42-no-stale";
+const CACHE_NAME = "yll-pos-first-clean-20260630-01";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", e => {});
