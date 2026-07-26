a4f98e0cce1d297dc4b03cbdb18e5d0c3d3515b8
okok6868 <fjun9198@gmail.com>
2026-07-26 17:06:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yll-pos-latest-boss-performance-20260726-02";
+const CACHE_NAME = "yll-pos-latest-ipad-order-confirm-20260726-03";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", e => {});
