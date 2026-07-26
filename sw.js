81ef3ec3840dfd0ecd50913f0eff00059030eb5c
okok6868 <fjun9198@gmail.com>
2026-07-26 13:14:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yll-pos-latest-product-alignment-20260726-01";
+const CACHE_NAME = "yll-pos-latest-boss-performance-20260726-02";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", e => {});
